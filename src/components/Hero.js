df652a80b2479f10d983781a79f6cb74b9d227d6
Aline Balogh <aline.balogh@wunderman.com>
2018-07-25 19:01:49 -0300

    Implentar estilos no Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,69 @@
 import React, {Component} from 'react'
-import {Stylesheet, css} from 'aphrodite'
+import { css} from 'emotion'
+import styled, {keyframes} from 'react-emotion'
 
 class Hero extends Component{
+
     render(){
-        
         const {
             classes,
             title,
             subtitle,
-            backgroungImg,
+            backgroundImg,
             minimumheight
         } = this.props
 
+        const HeroWrap = styled('div')`
+            display:flex;
+            justify-content: center;
+            align-items: center;
+        `
+        const Background = styled('div')`
+            background-color: rgba(36, 28, 28, 0.51);
+            background-image: url('${(props)=> backgroundImg}');  
+            height: ${(props)=> minimumheight};
+            width:100%;
+            background-position: 50% 75%;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+            background-blend-mode: multiply;
+            position: relative;
+            z-index:-1;
+        `
+        const type = keyframes`
+            from { width: 0;}
+        `
+        const Text = styled('p')`
+            text-align: center;
+            color: #e3dbdb;
+            font-size: 58px;
+            z-index: 10;
+            text-shadow: 0 4px 4px rgba(0, 0, 0, 0.4),
+                        0 4px 14px rgba(0, 0, 0, 0.1),
+                        0 0 24px rgba(0, 0, 0, 0.1);
+            :after{
+                display: block;
+                content: "What a time";
+                padding: 0;
+                margin:0;
+                text-align: center;
+                white-space: nowrap;
+                overflow: hidden;
+                font-size: 36px;
+                width: 100%;
+                animation-timing-function: ease-in-out;
+                animation: ${type} 5000ms steps(60, end);
+            }
+
+        `
+
         return (
-            <div className="wrap-hero hero-image">
-                <div >
-                    <p className="text">{title}<br /> </p>
-                </div>
-            </div>
+            <HeroWrap >
+                <Background >
+                    <Text>{title}<br /> </Text>
+                </Background>
+            </HeroWrap>
         )
     }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
 import Hero from '../components/Hero'
+import background from '../static/img/background.jpg'
 
 class IndexPage extends Component{
     render() {
         return (
             
             <div>
-                <Hero title="Hey. My name is Aline" />    
+                <Hero
+                    title="What a life"
+                    backgroundImg={background}
+                    minimumheight='80vh' />    
             </div>
         )
     }
